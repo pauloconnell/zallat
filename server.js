6a131e12cd99764b56d3fa237c136c3f40579865
Glitch (zallat) <none>
2021-10-19 03:42:59 +0000

    Ready for submission

--- server.js
+++ server.js
@@ -81,20 +81,31 @@ async function addStateData(state, dataArray, done) {
 }
 
 // this function will calculate max emmsions for all states in DB
-async function findMaxEmissions(done) {
+async function findMaxEmissions(toYear, fromYear, done) {
   try {
     //    var gotStateData=new stateDB({name:"CA", data:dataArray});
     var allStatesInfo = await stateDB.find({});
     var maxEmissionState = { state: "", emission: 0 };
-    console.log("state data is", allStatesInfo, JSON.stringify(allStatesInfo));
+    console.log(
+      "state data is",
+      allStatesInfo[0].name,
+      JSON.stringify(allStatesInfo[0])
+    );
     if (allStatesInfo.length == 0) done(null, "no info in DB");
     else {
       allStatesInfo.forEach(state => {
         state.data.forEach(year => {
-          if (year[1] > maxEmissionState.emission) {
-            maxEmissionState.state = state.name;
-            maxEmissionState.emission = year[1];
-            console.log("max is now ", JSON.stringify(maxEmissionState));
+          if (
+            parseInt(year[0], 10) >= fromYear &&
+            parseInt(year[0], 10) <= toYear
+          ) {
+           // console.log("checking : ", state.name, year);
+
+            if (year[1] >= maxEmissionState.emission) {
+              maxEmissionState.state = state.name;
+              maxEmissionState.emission = year[1];
+              console.log("max is now ", JSON.stringify(maxEmissionState));
+            }
           }
         });
       });
@@ -201,7 +212,8 @@ app.post("/API/cost", function(request, response) {
 
 // this is the endpoint that will hit DB to determine state with most emissions over this period
 app.post("/API/max", function(request, response) {
-  let answer = findMaxEmissions(function(err, result) {
+  var { toYear3, fromYear3 } = request.body;
+  let answer = findMaxEmissions(toYear3, fromYear3, function(err, result) {
     // add request.body.toDate3 & frontDate3
     if (err) {
       console.log(err);
@@ -222,12 +234,11 @@ app.post("/API/add", async function(request, response) {
     try {
       await checkForStateData(state, function(err, result) {
         if (err) {
-          console.log(err + "can't check DB for this state ", state);
-          haveData = "error checking for state in db"; //cancel below api call due to error
+          console.log(err + " error on this state ", state);
+          haveData =
+            "set to avoid api call on line 238 due to this unlikely error";
         } else {
-          //if (result) {
           console.log(result + "state data in DB?");
-          //return response.json(result);
           haveData = result;
         }
       });
@@ -248,24 +259,9 @@ app.post("/API/add", async function(request, response) {
         })
         .then(async myJson => {
           console.log("recieved: ", JSON.stringify(myJson.series[0].data));
-          //myJson.series[0].data.forEach(year => {
-          //  console.log("checking", year);
-          //    console.log("found year ", year);
-          //});
+
           let reply = myJson.series[0].data;
-          console.log(
-            "drilled into response object to get ",
-            JSON.stringify(reply)
-          );
-          // reply.unshift(["name", state]);
-          //let jsonReply = { ...reply };
           console.log("about to save to DB", JSON.stringify(reply));
-
-          //reply = "[name:" + JSON.stringify(myJson.series[0].name) + "]";
-          //reply += JSON.stringify(myJson.series[0].data);
-
-          //let replyJSON=JSON.parse(reply);
-          //check for state on DB
 
           //if state not here, add to DB
 
